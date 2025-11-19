970a2a3216c0a512e2aec398b37859ea023ceeb3
dauvannam1804 <dauvannam321@gmail.com>
2025-11-20 00:08:13 +0700

    feat: add save loss plot result function

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
+def plot_loss_curves(loss_history, horizons, save_dir=None):
+    import matplotlib.pyplot as plt
+    import os
 
-def plot_loss_curves(loss_history, horizons):
+    if save_dir:
+        os.makedirs(save_dir, exist_ok=True)
+
     for hz in horizons:
         plt.figure(figsize=(10, 6))
         for model in loss_history:
@@ -9,4 +13,7 @@ def plot_loss_curves(loss_history, horizons):
                 plt.plot(loss_history[model][hz]["val"], "--", label=f"{model} Val")
         plt.legend()
         plt.title(f"Loss curves ({hz})")
-        plt.show()
+
+        if save_dir:
+            plt.savefig(os.path.join(save_dir, f"loss_{hz}.png"))
+        plt.close()
